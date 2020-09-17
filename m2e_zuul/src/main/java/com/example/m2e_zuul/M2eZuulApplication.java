024cbe29752ca020fdf71b79631bdad5173bcfc8
lg900615 <601287853@qq.com>
2020-09-17 18:04:31 +0800

    单点

--- m2e_zuul/src/main/java/com/example/m2e_zuul/M2eZuulApplication.java
+++ m2e_zuul/src/main/java/com/example/m2e_zuul/M2eZuulApplication.java
@@ -2,14 +2,24 @@ package com.example.m2e_zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableFeignClients
+@EnableEurekaClient
 public class M2eZuulApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(M2eZuulApplication.class, args);
     }
 
+
+    @Bean
+    public AccessFilter accessFilter() {
+        return new AccessFilter();
+    }
 }
